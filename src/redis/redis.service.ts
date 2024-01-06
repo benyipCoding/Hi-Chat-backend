2e40011378a227757d637045ccd4e1aec2cc53f9
Ben Yip <ben_yip@126.com>
2024-01-06 16:25:53 +0800

    ...

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -18,6 +18,8 @@ export class RedisService
     this.redisClient = new Redis({
       host: process.env.REDIS_HOST,
       port: +process.env.REDIS_PORT,
+      password:
+        process.env.STAGE === 'pro' ? process.env.REDIS_PASSWORD : undefined,
     });
   }
 }
